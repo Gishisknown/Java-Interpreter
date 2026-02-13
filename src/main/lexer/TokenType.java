6b0820259384d1d19efc0c4ad31797b7dd2f2ff7
Gishisknown <gishisknown@gmail.com>
2026-02-13 18:56:59 -0500

    initial setup 2

---
+++ src/main/lexer/TokenType.java
@@ -0,0 +1,14 @@
+public enum TokenType {
+    INT,
+    IDENTIFIER,
+    NUMBER,
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    EQUALS,
+    SEMICOLON,
+    PRINT,
+    LEFT_PAREN,
+    RIGHT_PAREN
+}
